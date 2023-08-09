1ae306ef05fa7b97c91e7531a7808104991d95a3
duyuboCT@163.com <duyuboCT@163.com>
2023-08-09 18:49:06 +0800

    init

--- DataCollectify02/spiders/tanyancha.py
+++ DataCollectify02/spiders/tanyancha.py
@@ -1,10 +1,34 @@
 import scrapy
 
-# python -m scrapy genspider tanyancha www.tianyancha.com
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from  selenium.webdriver.chrome.options import Options    # 使用无头浏览器
+import scrapy
+
 class TanyanchaSpider(scrapy.Spider):
     name = "tanyancha"
     start_urls = ["https://www.tianyancha.com"]
 
-    def parse(self, response):
+    # 实例化一个浏览器对象
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        super().__init__()
 
-        pass
+    def parse(self,response):
+        url = self.start_urls[0]
+        response = scrapy.Request(url, callback=self.parse_main)
+        yield response
+
+    # 整个爬虫结束后关闭浏览器
+    def close(self, spider):
+        self.driver.quit()
+
+
+
+    # 访问主页的url, 拿到对应板块的response
+    def parse_main(self, response):
+        div_list = response.xpath("//div[@class='ns_area list']/ul/li/a/@href").extract()
+        index_list = [3,4,6,7]
+        for index in index_list:
+            response = scrapy.Request(div_list[index],callback=self.parse_detail)
+            yield response
---
+++ tests/selenium01.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+"""
+    url：https://www.tianyancha.com/
+    测试登录并滑动获取cookie信息
+"""
+driver = webdriver.Chrome()
+driver.get('https://www.tianyancha.com/')
+driver.find_element(by="xpath", value='//*[@id="page-container"]/div[1]/div/div[1]/div[2]/div/div[5]/span').click()
+time.sleep(1)
+driver.find_element(by="xpath", value='//*[@id="J_Modal_Container"]/div/div/div[2]/div/div[2]/div/div/div[2]').click()
+time.sleep(1)
+driver.find_element(by="xpath", value='//*[@id="J_Modal_Container"]/div/div/div[2]/div/div[2]/div/div/div[3]/div[1]/div[2]').click()
+time.sleep(1)
+
+# 定位到用户名输入框并输入文本
+element = driver.find_element(by="xpath", value='//*[@id="J_Modal_Container"]/div/div/div[2]/div/div[2]/div/div/div[3]/div[2]/div[1]/input')
+element.send_keys(15201443697)
+element = driver.find_element(by="xpath", value='//*[@id="J_Modal_Container"]/div/div/div[2]/div/div[2]/div/div/div[3]/div[2]/div[2]/input')
+element.send_keys("1234qwer")
+
+# 勾选同意
+driver.find_element(by="xpath", value='//*[@id="J_Modal_Container"]/div/div/div[2]/div/div[2]/div/div/div[3]/div[3]/input').click()
+
+# 点击登录
+driver.find_element(by="xpath", value='//*[@id="J_Modal_Container"]/div/div/div[2]/div/div[2]/div/div/div[3]/div[2]/button').click()
+time.sleep(2)
+
+# 找到滑块的位置
+slider = driver.find_element(By.CSS_SELECTOR,'body > div.gt_holder.gt_popup.gt_animate.gt_show > div.gt_popup_wrap > div.gt_popup_box > div.gt_slider > div.gt_slider_knob.gt_show')
+slider.click()
+time.sleep(4)
+
+# 26块
+driver.find_element(by="xpath", value='/html/body/div[4]/div[2]/div[2]/div[1]/div[2]/div[1]').screenshot("../down/img/div_screenshot03.png")
+
+# 滑动注释
+# action = ActionChains(driver)
+# action.click_and_hold(slider).perform()
+# action.move_by_offset(200, 0).perform()
+# action.release().perform()
+
+time.sleep(5)
+driver.quit()
+# action = ActionChains(driver)
+# action.click_and_hold(slider).perform()
+# action.move_by_offset(200, 0).perform()
+# action.release().perform()
+#
+# time.sleep(5)
+# driver.quit()
